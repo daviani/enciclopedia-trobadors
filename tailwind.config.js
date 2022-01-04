27d8a7a489925a975520c7605dc1c7e161d70a4d
Daviani <contact@daviani.dev>
2022-01-04 15:19:07 +0100

    :wrench: style: Add main colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	darkMode: true,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-snow': '#ECEFF4',
+				'secondary-snow': '#D8DEE9',
+				'primary-night': '#2E3440',
+				'secondary-night': '#4C566A',
+				primary: '#81A1C1',
+				secondary: '#8FBCBB',
+				error: '#BF616A',
+				warning: '#EBCB8B',
+				success: '#A3BE8C',
+			},
+		},
 	},
 	plugins: [],
 };
